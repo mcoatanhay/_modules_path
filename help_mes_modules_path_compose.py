1ceb53e7a4c9346b8388b090bd3b53cbb43d6343
mcoatanhay@gmail.com <mcoatanhay@gmail.com>
2020-02-21 21:12:37 +0100

    premier commit

---
+++ help_mes_modules_path_compose.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Fichier: help_mes_modules_path_compose.py
+# Auteur: Marc COATANHAY
+
+"""
+    Module pour écrire automatique l'aide d'un module.
+"""
+
+# Import des modules
+import mes_modules_path
+from repertoire import filerep
+import sys
+
+# Définitions constantes et variables globales
+filerep()
+file = open('help.txt', 'w')
+sys.stdout = file
+help(mes_modules_path)
+file.close()
+sys.stdout = sys.__stdout__
+
+# Définitions fonctions et classes
+
---
+++ mes_modules_path.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Fichier: mes_modules_path.py
+# Auteur: Marc COATANHAY
+
+"""
+    Ajout du path nécessaire à l'importation des modules perso.
+    Changer la valeur de répertoire avec l'adresse à laquelle se trouvent
+    les modules perso.
+"""
+
+# Import des modules
+import sys
+
+# Définitions constantes et variables globales
+repertoire = 'c:\\users\\mc\\mu_code\\_mes_modules\\'
+sys.path.append(repertoire)
+print('mes modules path :', repertoire)
+
+# Définitions fonctions et classes
